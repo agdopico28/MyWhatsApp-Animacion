313b8d777a947222437ceb609b31a09d59128e2e
alu10701951 <alugar1951@ieselcaminas.org>
2023-11-14 20:44:40 +0100

    Animaciones

--- app/src/main/java/com/example/mywhatsapp/Novedades.kt
+++ app/src/main/java/com/example/mywhatsapp/Novedades.kt
@@ -6,6 +6,7 @@ import androidx.compose.animation.graphics.res.rememberAnimatedVectorPainter
 import androidx.compose.animation.graphics.vector.AnimatedImageVector
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -13,13 +14,17 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 
 @Composable
 fun Novedades(){
-    Icono()
+    Box(contentAlignment = Alignment.Center){
+        Icono()
+    }
+
 }
 
 @OptIn(ExperimentalAnimationGraphicsApi::class)
@@ -30,6 +35,7 @@ fun Icono(){
         )
     var atEnd by remember { mutableStateOf(false) }
 
+
         Image(
             painter = rememberAnimatedVectorPainter(image, atEnd),
             contentDescription = "VectorDrawable",
@@ -37,6 +43,7 @@ fun Icono(){
                 atEnd = !atEnd
             },
             contentScale = ContentScale.Crop,
+            alignment = Alignment.Center
         )
 
 }
